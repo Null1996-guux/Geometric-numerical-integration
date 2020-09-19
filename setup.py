89bd6eba8f6d559e4729b391e37c7d5fe71a887f
Null1996-guux <60610570+Null1996-guux@users.noreply.github.com>
2020-09-19 21:38:56 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 10:27:43 2020
+
+@author: Math
+"""
+
+from distutils.core import setup
+
+setup(name='geometric_integration', version='1.0' ,packages=['geometric_integ', 'geometric_integ.example', \
+                                                             'geometric_integ.integrate_method', 'geometric_integ.model_data'],)
